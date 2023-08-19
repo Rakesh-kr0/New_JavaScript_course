1bc5e51fe130143d4c9b85b5add2f2195743c4da
Rakesh kumar <97838721+Rakesh-kr0@users.noreply.github.com>
2023-08-19 03:31:35 +0000

    basics variable let,const,var

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId= 01;
+let accountName="RAKESH KUMAR SINGH"
+let accountNumber
+let accountState="Bihar";
+var accountCountry="INDIA";
+
+
+console.table([accountId,accountName,accountNumber,accountState,accountCountry])
