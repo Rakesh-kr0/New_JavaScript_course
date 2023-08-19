fa1633d86a6f5b8607ac31bd794cac98f3fa79ba
Rakesh kumar <97838721+Rakesh-kr0@users.noreply.github.com>
2023-08-19 10:03:07 +0000

    Comparison of datatype

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+console.log(2>1)//true
+console.log(2<1)//false
+console.log(2=1)//false
+console.log(2>=1)//false
+console.log(2<=1)//false
+
+console.log("2" > 1);//true
+console.log("02" > 1)//true
+
+console.log(null>0)//false
+console.log(null==0)//false
+console.log(null>=0)//true
+console.log(null<=0)//true
+
+
+/////////////////////////////////////////////////////
+
+// === strict equality => both value and data type are must be same//
+
+console.log("2"===2)// false
+console.log("2"==="2")// true
+console.log(2==="2")// true
