4e159c66d446023dbbe09d4ad7d37097c3001255
Rakesh kumar <97838721+Rakesh-kr0@users.noreply.github.com>
2023-08-19 06:13:54 +0000

    conversion-type

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,27 @@
+let score= "33abc";  // null=>output 0// undefined=> NAN// 
+//Boolean true=>1, false=>0//
+//string=> "Rakesh "=>NAN
+console.log(typeof score)//type of 
+console.log(typeof(score))// method 
+
+let valueInNumber=Number(score);
+console.log( typeof valueInNumber); // number
+console.log(valueInNumber);//NAN
+
+/////////////////////////////
+//"33"=>33
+//"22abs"=> Nan
+//true=>1;false=>0;
+//""=> false;
+// "Rakesh"=> true;
+////////////////////////////////////
+
+
+let someNumber=44;
+let stringNumber= String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+///////////////////////////////
+
