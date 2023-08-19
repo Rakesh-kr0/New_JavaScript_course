bd0e05b087aabed724c0a67a8edb7d13097174c2
Rakesh kumar <97838721+Rakesh-kr0@users.noreply.github.com>
2023-08-19 05:10:38 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"// treat all javascript cose in new vwesion//
+// aleart(3+3) right now we are using node js not browser//
+
+
+
+// numbers => 2 to the power 53
+//BigInt=> use a large number integer 
+
+let name="Rakesh"; //=> string data type//
+let age = 18; // number data type//
+let loginId  =true//=>boolean// true/false//
+let stuentName;// undefined//
+let state=null;// null=>empty//
+
+
+//primitive data type//
+
+//1.string => "Rakesh";//
+//2.number => 18//
+//3.boolean => true/false//
+//4.null => empty
+//5.undefined => A variable that has not been assigned a value//
+//Symbol => unique//
+
+
+
+//non primitive data type//
+
+//1.Object//
+//2.Array //
+//3. 
+
+
+//////////////////////////////////////
+
+console.log(typeof undefined);// undefined//
+console.log(typeof null);// object//
