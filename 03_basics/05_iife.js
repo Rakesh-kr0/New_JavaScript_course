dfd5a4f19045f807ad99081bb687e948d8438340
Rakesh kumar <97838721+Rakesh-kr0@users.noreply.github.com>
2023-08-23 02:24:39 +0000

    iife in javascript

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression (IIFE)
+
+// named IIFE
+(function chai(){
+    console.log("DB CONNECTED");
+})();// must important to use ;
+
+
+// simple arrow
+
+((name) =>{
+    console.log(`DB CONNECTED TWO`)
+
+})("Rakesh")
